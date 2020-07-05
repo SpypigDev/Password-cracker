b4b77d3ce0d5ce98950dd8ca33c8ffe35c2e95eb
spypig23 <31124786+spypig23@users.noreply.github.com>
2020-07-05 12:25:26 +1000

    Working on fixing glitch with counting

--- main.bat
+++ main.bat
@@ -1,6 +1,6 @@
 @echo off
 
-set /p code=How many letters we trying to crack: 
+set /p code=How many letters we trying to crack:
 
 goto pre
 
@@ -9,18 +9,12 @@ goto pre
 
   echo Set WshShell = WScript.CreateObject("WScript.Shell")> code.vbs
   echo Set objShell = CreateObject("WScript.Shell")>> code.vbs
-  echo var1 = 1>> code.vbs
+  echo var1 = 1 >> code.vbs
   echo WScript.Sleep 5000>> code.vbs
   echo.>> code.vbs
   echo Do>> code.vbs
   echo WScript.Sleep 1000>> code.vbs
   echo WshShell.SendKeys "admin">> code.vbs
-  echo If var1 = 27 Then>> code.vbs
-  echo    var1 = 1>> code.vbs
-  echo var2 = var2 + "1">> code.vbs
-  echo var3 = var3 + "1">> code.vbs
-  echo var4 = var4 + "1">> code.vbs
-  echo End If>> code.vbs
   echo.>> code.vbs
   echo With CreateObject("WScript.Shell")>> code.vbs
   echo     .Run "nircmd setcursor 761 334‬", 0, True>> code.vbs
@@ -31,7 +25,20 @@ echo.>> code.vbs
 goto start
 
 :start
+set x=-1
+set /A result=%x% + %code%
 (
+   echo var%code% = var%code% + "1"
+   echo If var%code% = 27 Then
+   echo    var%code% = 1 
+   echo code = %result%
+   echo var%code% = var%code% + 1
+) >> code.vbs 
+set x=1
+set /A result=%x% + %code%
+(
+   echo code = %result%
+   echo End If
   echo If var%code% = "1" Then
 echo WshShell.SendKeys "a"
 echo ElseIf var%code% = "2" Then
@@ -92,7 +99,89 @@ set x=-1
 set /A result=%x% + %code%
 
 set code=%result%
+if %code%==0 (goto final) else (goto start3)
+
+
+:start3
+set x=-1
+set /A result=%x% + %code%
+(
+   echo var%code% = var%code% + "1"
+   echo If var%code% = 27 Then
+   echo    var%code% = 1 
+   echo code = %result%
+   echo var%code% = var%code% + 1
+) >> code.vbs 
+set x=1
+set /A result=%x% + %code%
+(
+   echo code = %result%
+   echo End If
+  echo If var%code% = "1" Then
+echo WshShell.SendKeys "a"
+echo ElseIf var%code% = "2" Then
+echo WshShell.SendKeys "b"
+echo ElseIf var%code% = "3" Then
+echo WshShell.SendKeys "c"
+echo ElseIf var%code% = "4" Then
+echo WshShell.SendKeys "d"
+echo ElseIf var%code% = "5" Then
+echo WshShell.SendKeys "e"
+echo ElseIf var%code% = "6" Then
+echo WshShell.SendKeys "f"
+echo ElseIf var%code% = "7" Then
+echo WshShell.SendKeys "g"
+echo ElseIf var%code% = "8" Then
+echo WshShell.SendKeys "h"
+echo ElseIf var%code% = "9" Then
+echo WshShell.SendKeys "i"
+echo ElseIf var%code% = "10" Then
+echo WshShell.SendKeys "j"
+echo ElseIf var%code% = "11" Then
+echo WshShell.SendKeys "k"
+echo ElseIf var%code% = "12" Then
+echo WshShell.SendKeys "l"
+echo ElseIf var%code% = "13" Then
+echo WshShell.SendKeys "m"
+echo ElseIf var%code% = "14" Then
+echo WshShell.SendKeys "n"
+echo ElseIf var%code% = "15" Then
+echo WshShell.SendKeys "o"
+echo ElseIf var%code% = "16" Then
+echo WshShell.SendKeys "p"
+echo ElseIf var%code% = "17" Then
+echo WshShell.SendKeys "q"
+echo ElseIf var%code% = "18" Then
+echo WshShell.SendKeys "r"
+echo ElseIf var%code% = "19" Then
+echo WshShell.SendKeys "s"
+echo ElseIf var%code% = "20" Then
+echo WshShell.SendKeys "t"
+echo ElseIf var%code% = "21" Then
+echo WshShell.SendKeys "u"
+echo ElseIf var%code% = "22" Then
+echo WshShell.SendKeys "v"
+echo ElseIf var%code% = "23" Then
+echo WshShell.SendKeys "w"
+echo ElseIf var%code% = "24" Then
+echo WshShell.SendKeys "x"
+echo ElseIf var%code% = "25" Then
+echo WshShell.SendKeys "y"
+echo ElseIf var%code% = "26" Then
+echo WshShell.SendKeys "z"
+echo code=%code% - "1"
+echo var%code%= var%code% + "1"
+echo code=%code% + "1"
+echo End If
+  echo.
+) >> code.vbs
+
+set x=-1
+set /A result=%x% + %code%
+
+set code=%result%
 goto start2
+
 
 :start2
 if %code%==0 (goto final) else (goto start)
@@ -103,5 +192,5 @@ echo WshShell.SendKeys "{ENTER}"
 echo var1 = var1 - 1
 echo loop
 ) >> code.vbs
-pause
-run code
+timeout /t 1 >nul
+start code.vbs
